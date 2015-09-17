b01106424c5019d960d0ce7e8c0667f544f3c2d6
Marc Clint Dion <marcclintdion@Marcs-iMac.local>
2015-09-17 04:39:15 -0700

    init

---
+++ lightPosition.cpp
@@ -0,0 +1,6 @@
+  GLfloat     torso_POSITION[]                  = {0, 0, 0, 1};
+  GLfloat     horse_BLENDER_ATTENUATION         =  0.85;
+  GLfloat     horse_BLENDER_scale_UV_MAP          =  2;
+  var     eyePosition[]                         = {-7.72412, 0, 17.8886, 1};
+  var     look_LEFT_RIGHT                       =  -22.4;
+  var     look_UP_DOWN                          =  25;
